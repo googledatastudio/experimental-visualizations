52deed817dcaee4d41df6b0a8ff55188040845f0
Matt Hamrick <matthewjhamrick@gmail.com>
2018-05-23 12:06:29 -0700

    Added react example.

---
+++ examples/using-react/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+VIS_NAME="$1"
+GCS_BUCKET="$2"
+
+yarn install
+yarn build
+mkdir -p deploy
+cp build/static/js/*.js "deploy/$VIS_NAME.js"
+cp "src/$VIS_NAME.json" "deploy/$VIS_NAME.json"
+cp "src/$VIS_NAME.css" "deploy/$VIS_NAME.css"
+
+/google/data/ro/projects/cloud/bigstore/tools/gsutil.par cp -a public-read deploy/* "gs://$GCS_BUCKET"
