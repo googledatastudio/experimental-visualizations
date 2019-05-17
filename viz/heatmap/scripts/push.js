cf072631eea3509cc2825f1b67e55991c9fa0533
yulan <yulan.lin.2014@gmail.com>
2019-05-17 18:37:07 -0500

    fixing submodule issues

---
+++ viz/heatmap/scripts/push.js
@@ -0,0 +1,11 @@
+const shelljs = require('shelljs');
+const bluebird = require('bluebird');
+
+const deploy = async (bucketName) => {
+  shelljs.cd('build');
+  const asyncExec = bluebird.promisify(shelljs.exec);
+  await asyncExec(`gsutil cp -a public-read * ${bucketName}`);
+  console.log(`Viz deployed to: ${bucketName}`);
+};
+
+deploy('gs://public-community-viz-showcase-reports/heatmap');
