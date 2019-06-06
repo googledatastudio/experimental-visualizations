ea702d53ff260d2fdf28d78a11d4f2c1f15f4d8b
Yulan Lin <yulan.lin.2014@gmail.com>
2019-06-06 14:36:18 -0700

    added treemap

---
+++ viz/treemap/src/localMessage.js
@@ -0,0 +1,201 @@
+export const message = {
+  tables: {
+    DEFAULT: [
+      {
+        levels: ['Second Platform', 'desktop'],
+        area: [2014545],
+      },
+      {
+        levels: ['First Platform', 'desktop'],
+        area: [676227],
+      },
+      {
+        levels: ['Second Platform', 'tablet'],
+        area: [196255],
+      },
+      {
+        levels: ['Second Platform', 'mobile'],
+        area: [165793],
+      },
+      {
+        levels: ['First Platform', 'mobile'],
+        area: [70575],
+      },
+      {
+        levels: ['First Platform', 'tablet'],
+        area: [53869],
+      },
+    ],
+  },
+  fields: {
+    levels: [
+      {
+        id: 'qt_baidtfc1xb',
+        name: 'Website',
+        type: 'TEXT',
+        concept: 'DIMENSION',
+      },
+      {
+        id: 'qt_caidtfc1xb',
+        name: 'Device',
+        type: 'TEXT',
+        concept: 'DIMENSION',
+      },
+    ],
+    area: [
+      {
+        id: 'qt_daidtfc1xb',
+        name: 'Visits',
+        type: 'NUMBER',
+        concept: 'METRIC',
+      },
+    ],
+  },
+  style: {
+    showL1Labels: {
+      value: false,
+      defaultValue: false,
+    },
+    showL2Labels: {
+      value: false,
+      defaultValue: false,
+    },
+    textXOffset: {
+      value: 1,
+      defaultValue: 0,
+    },
+    textYOffset: {
+      value: 1,
+      defaultValue: 0,
+    },
+    fontSize: {
+      value: 10,
+      defaultValue: 10,
+    },
+    fontColor: {
+      value: {
+        color: '#000000',
+      },
+      defaultValue: '#000000',
+    },
+    fontFamily: {
+      value: 'Arial',
+      defaultValue: 'Arial',
+    },
+  },
+  theme: {
+    themeFillColor: {
+      color: '#fff',
+      opacity: 1,
+    },
+    themeFontColor: {
+      color: '#616161',
+      opacity: 1,
+    },
+    themeFontFamily: 'Roboto',
+    themeAccentFillColor: {
+      color: '#4285F4',
+      opacity: 1,
+    },
+    themeAccentFontColor: {
+      color: '#EFEFEF',
+      opacity: 1,
+    },
+    themeAccentFontFamily: 'Roboto',
+    themeSeriesColor: [
+      {
+        color: '#4285F4',
+        opacity: 1,
+      },
+      {
+        color: '#DB4437',
+        opacity: 1,
+      },
+      {
+        color: '#F4B400',
+        opacity: 1,
+      },
+      {
+        color: '#0F9D58',
+        opacity: 1,
+      },
+      {
+        color: '#AB47BC',
+        opacity: 1,
+      },
+      {
+        color: '#00ACC1',
+        opacity: 1,
+      },
+      {
+        color: '#FF7043',
+        opacity: 1,
+      },
+      {
+        color: '#9E9D24',
+        opacity: 1,
+      },
+      {
+        color: '#5C6BC0',
+        opacity: 1,
+      },
+      {
+        color: '#F06292',
+        opacity: 1,
+      },
+      {
+        color: '#00796b',
+        opacity: 1,
+      },
+      {
+        color: '#c2185b',
+        opacity: 1,
+      },
+      {
+        color: '#7e57c2',
+        opacity: 1,
+      },
+      {
+        color: '#03a9f4',
+        opacity: 1,
+      },
+      {
+        color: '#8bc34a',
+        opacity: 1,
+      },
+      {
+        color: '#fdd835',
+        opacity: 1,
+      },
+      {
+        color: '#fb8c00',
+        opacity: 1,
+      },
+      {
+        color: '#8d6e63',
+        opacity: 1,
+      },
+      {
+        color: '#9e9e9e',
+        opacity: 1,
+      },
+      {
+        color: '#607d8b',
+        opacity: 1,
+      },
+    ],
+    themeIncreaseColor: {
+      color: '#388e3c',
+      opacity: 1,
+    },
+    themeDecreaseColor: {
+      color: '#f44336',
+      opacity: 1,
+    },
+    themeGridColor: {
+      color: '#e9e9e9',
+      opacity: 1,
+    },
+  },
+  interactions: {},
+};
