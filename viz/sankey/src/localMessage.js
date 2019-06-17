1f8e67fef7c0522b7caef7e510e0e02803d660d4
Yulan Lin <yulan.lin.2014@gmail.com>
2019-06-17 11:34:10 -0700

    oh hey sankey is added

---
+++ viz/sankey/src/localMessage.js
@@ -0,0 +1,271 @@
+export const message = {
+  tables: {
+    DEFAULT: [
+      {
+        dimensions: ['Afternoon', 'Sunday'],
+        metrics: [25],
+      },
+      {
+        dimensions: ['Afternoon', 'Saturday'],
+        metrics: [25],
+      },
+      {
+        dimensions: ['Afternoon', 'Tuesday'],
+        metrics: [23],
+      },
+      {
+        dimensions: ['Afternoon', 'Wednesday'],
+        metrics: [22],
+      },
+      {
+        dimensions: ['Afternoon', 'Friday'],
+        metrics: [20],
+      },
+      {
+        dimensions: ['Afternoon', 'Thursday'],
+        metrics: [20],
+      },
+      {
+        dimensions: ['Afternoon', 'Monday'],
+        metrics: [20],
+      },
+      {
+        dimensions: ['Morning', 'Tuesday'],
+        metrics: [19],
+      },
+      {
+        dimensions: ['Evening', 'Monday'],
+        metrics: [18],
+      },
+      {
+        dimensions: ['Morning', 'Monday'],
+        metrics: [18],
+      },
+      {
+        dimensions: ['Morning', 'Sunday'],
+        metrics: [18],
+      },
+      {
+        dimensions: ['Morning', 'Thursday'],
+        metrics: [17],
+      },
+      {
+        dimensions: ['Evening', 'Sunday'],
+        metrics: [17],
+      },
+      {
+        dimensions: ['Morning', 'Wednesday'],
+        metrics: [17],
+      },
+      {
+        dimensions: ['Morning', 'Saturday'],
+        metrics: [16],
+      },
+      {
+        dimensions: ['Morning', 'Friday'],
+        metrics: [15],
+      },
+      {
+        dimensions: ['Evening', 'Tuesday'],
+        metrics: [15],
+      },
+      {
+        dimensions: ['Evening', 'Wednesday'],
+        metrics: [13],
+      },
+      {
+        dimensions: ['Evening', 'Friday'],
+        metrics: [13],
+      },
+      {
+        dimensions: ['Evening', 'Saturday'],
+        metrics: [13],
+      },
+      {
+        dimensions: ['Evening', 'Thursday'],
+        metrics: [12],
+      },
+    ],
+  },
+  fields: {
+    dimensions: [
+      {
+        id: 'qt_7luivd0mxb',
+        name: 'Time of Day',
+        type: 'TEXT',
+        concept: 'DIMENSION',
+      },
+      {
+        id: 'qt_8luivd0mxb',
+        name: 'Day',
+        type: 'TEXT',
+        concept: 'DIMENSION',
+      },
+    ],
+    metrics: [
+      {
+        id: 'qt_9luivd0mxb',
+        name: 'Temperature',
+        type: 'NUMBER',
+        concept: 'METRIC',
+      },
+    ],
+  },
+  style: {
+    node_color: {
+      value: {
+        color: '#000000',
+      },
+      defaultValue: '#000000',
+    },
+    link_color: {
+      value: {
+        color: '#000',
+      },
+      defaultValue: '#000',
+    },
+    link_opacity: {
+      value: 0.2,
+      defaultValue: 0.2,
+    },
+    show_labels: {},
+    node_font_size: {
+      value: 20,
+      defaultValue: 20,
+    },
+    node_font_color: {
+      value: {
+        color: '#000000',
+      },
+      defaultValue: '#000000',
+    },
+    left_offset: {
+      value: 20,
+      defaultValue: 20,
+    },
+    right_offset: {
+      value: 20,
+      defaultValue: 20,
+    },
+  },
+  theme: {
+    themeFillColor: {
+      color: '#fff',
+      opacity: 1,
+    },
+    themeFontColor: {
+      color: '#616161',
+      opacity: 1,
+    },
+    themeFontFamily: 'Roboto',
+    themeAccentFillColor: {
+      color: '#4285F4',
+      opacity: 1,
+    },
+    themeAccentFontColor: {
+      color: '#EFEFEF',
+      opacity: 1,
+    },
+    themeAccentFontFamily: 'Roboto',
+    themeSeriesColor: [
+      {
+        color: '#4285F4',
+        opacity: 1,
+      },
+      {
+        color: '#DB4437',
+        opacity: 1,
+      },
+      {
+        color: '#F4B400',
+        opacity: 1,
+      },
+      {
+        color: '#0F9D58',
+        opacity: 1,
+      },
+      {
+        color: '#AB47BC',
+        opacity: 1,
+      },
+      {
+        color: '#00ACC1',
+        opacity: 1,
+      },
+      {
+        color: '#FF7043',
+        opacity: 1,
+      },
+      {
+        color: '#9E9D24',
+        opacity: 1,
+      },
+      {
+        color: '#5C6BC0',
+        opacity: 1,
+      },
+      {
+        color: '#F06292',
+        opacity: 1,
+      },
+      {
+        color: '#00796b',
+        opacity: 1,
+      },
+      {
+        color: '#c2185b',
+        opacity: 1,
+      },
+      {
+        color: '#7e57c2',
+        opacity: 1,
+      },
+      {
+        color: '#03a9f4',
+        opacity: 1,
+      },
+      {
+        color: '#8bc34a',
+        opacity: 1,
+      },
+      {
+        color: '#fdd835',
+        opacity: 1,
+      },
+      {
+        color: '#fb8c00',
+        opacity: 1,
+      },
+      {
+        color: '#8d6e63',
+        opacity: 1,
+      },
+      {
+        color: '#9e9e9e',
+        opacity: 1,
+      },
+      {
+        color: '#607d8b',
+        opacity: 1,
+      },
+    ],
+    themeIncreaseColor: {
+      color: '#388e3c',
+      opacity: 1,
+    },
+    themeDecreaseColor: {
+      color: '#f44336',
+      opacity: 1,
+    },
+    themeGridColor: {
+      color: '#e9e9e9',
+      opacity: 1,
+    },
+  },
+  interactions: {
+    onClick: {
+      value: {},
+      supportedActions: ['FILTER'],
+    },
+  },
+};
