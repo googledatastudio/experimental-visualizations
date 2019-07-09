085e2b57b98c9c8e9f86e6ef9d14d91d8d14ac35
Yulan Lin <yulan.lin.2014@gmail.com>
2019-07-09 16:16:09 -0700

    fixed css and cleaned up margins

--- viz/sankey/src/index.js
+++ viz/sankey/src/index.js
@@ -34,9 +34,21 @@ const parseData = data => {
   return {nodes, links};
 };
 
+const styleVal = (message, styleId) => {
+  // to account for color styling
+  if (
+    message.style[styleId].value !== undefined &&
+    typeof message.style[styleId].value === 'object'
+  ) {
+    return message.style[styleId].value.color;
+  }
+  return message.style[styleId].value !== undefined
+    ? message.style[styleId].value
+    : message.style[styleId].defaultValue;
+};
+
 const draw = message => {
   const sankeyData = parseData(message.tables.DEFAULT);
-  const style = message.style;
 
   // remove the canvas if it exists
   d3.select('body')
@@ -48,27 +60,10 @@ const draw = message => {
     .remove();
 
   // set margins
-  var margin = {left: 20, right: 50, top: 20, bottom: 20};
-  var padding = {left: 20, right: 50, top: 20, bottom: 20};
-  // get the width and the height of the iframe
-  var width = dscc.getWidth() - margin.left - margin.right;
-  var height = dscc.getHeight() - margin.top - margin.bottom;
 
-  const onClick = d => {
-    var FILTER = dscc.InteractionType.FILTER;
-    const fields = message.fields;
-    const dimId1 = fields['dimensions'][0].id;
-    const dimId2 = fields['dimensions'][1].id;
-    const sourceVal = d.source.id;
-    const targetVal = d.target.id;
-    console.log(fields);
-    var intxnData = {
-      concepts: [dimId1, dimId2],
-      values: [[sourceVal, targetVal]],
-    };
-    dscc.sendInteraction('onClick', FILTER, intxnData);
-    console.log(intxnData);
-  };
+  // get the width and the height of the iframe
+  var width = dscc.getWidth();
+  var height = dscc.getHeight();
 
   function onHover(d) {
     d3.select(this).attr('stroke-opacity', 1);
@@ -79,22 +74,21 @@ const draw = message => {
   }
 
   // set up the canvas space
+  const yMargin = 5;
+
   var svg = d3
     .select('body')
     .append('svg')
-    .attr('transform', 'translate(30, 30)')
-    .attr('width', width - margin.left)
-    .attr('height', height - margin.top);
+    .attr('width', width)
+    .attr('height', height - yMargin * 2)
+    .attr('transform', `translate(0, -${yMargin})`);
 
   // configure extent to use margin
   var sankey = d3
     .sankey()
     .nodeWidth(15)
-    .nodePadding(10)
-    .size([
-      width - padding.left - padding.right,
-      height - padding.top - padding.bottom,
-    ]);
+    .nodePadding(yMargin / 2)
+    .size([width, height - yMargin * 2]);
 
   sankey(sankeyData);
 
@@ -118,21 +112,15 @@ const draw = message => {
       return d.y1 - d.y0;
     })
     .attr('width', sankey.nodeWidth())
-    .style(
-      'fill',
-      style.node_color.value ? style.node_color.value.color : 'black'
-    );
+    .style('fill', styleVal(message, 'node_color'));
 
   // draw the links
   const links = svg
     .append('g')
     .attr('fill', 'none')
     .attr('transform', 'translate(0, 10)')
-    .attr(
-      'stroke',
-      style.link_color.value ? style.link_color.value.color : 'black'
-    )
-    .attr('stroke-opacity', Number(style.link_opacity.value))
+    .attr('stroke', styleVal(message, 'link_color'))
+    .attr('stroke-opacity', Number(styleVal(message, 'link_opacity')))
     .selectAll('path')
     .data(sankeyData.links)
     .enter()
@@ -141,17 +129,15 @@ const draw = message => {
     .attr('stroke-width', function(d) {
       return d.width;
     })
-    .on('click', onClick)
     .on('mouseover', onHover)
     .on('mouseout', leaveHover);
   // if I should show labels
-  if (style.show_labels) {
+  if (styleVal(message, 'show_labels')) {
     // add the "left" side labels
     svg
       .append('g')
-      .attr('transform', 'translate(0, 10)')
-      .style('font-size', style.node_font_size)
-      .style('fill', style.node_font_color.color)
+      .style('font-size', styleVal(message, 'node_font_size'))
+      .style('fill', styleVal(message, 'node_font_color'))
       .selectAll('text')
       .data(sankeyData.nodes)
       .enter()
@@ -160,9 +146,7 @@ const draw = message => {
         return d.x0 < width / 2;
       })
       .attr('x', function(d) {
-        return style.left_offset.value
-          ? d.x0 + parseInt(style.left_offset.value, 10)
-          : d.x0 + margin.right / 2;
+        return d.x0 + parseInt(styleVal(message, 'left_offset'));
       })
       .attr('y', function(d) {
         return (d.y0 + d.y1) / 2;
@@ -174,9 +158,8 @@ const draw = message => {
     // add the "right" side labels
     svg
       .append('g')
-      .attr('transform', 'translate(0, 10)')
-      .style('font-size', style.node_font_size)
-      .style('fill', style.node_font_color.color)
+      .style('font-size', styleVal(message, 'node_font_size'))
+      .style('fill', styleVal(message, 'node_font_color'))
       .selectAll('text')
       .data(sankeyData.nodes)
       .enter()
@@ -185,9 +168,7 @@ const draw = message => {
         return d.x0 > width / 2;
       })
       .attr('x', function(d) {
-        return style.right_offset.value
-          ? d.x0 - +style.right_offset.value
-          : d.x0 - margin.right / 2;
+        return d.x0 - parseInt(styleVal(message, 'right_offset'));
       })
       .attr('y', function(d) {
         return (d.y0 + d.y1) / 2;
