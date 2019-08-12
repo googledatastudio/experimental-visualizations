3586da40caa9681ba80907a7d6179d5d1b8fc31b
Ralph Spandl <ralph@r42.ca>
2019-08-12 15:22:19 -0400

    Sunburst with Prettier formatting

--- viz/sunburst-drilldown/sunburst/src/sunburst-data.js
+++ viz/sunburst-drilldown/sunburst/src/sunburst-data.js
@@ -1,102 +1,102 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const d3 = Object.assign(
-    {},
-    require("d3-array"),
-    require("d3-selection"),
-    require("d3-hierarchy")
+  {},
+  require("d3-array"),
+  require("d3-selection"),
+  require("d3-hierarchy")
 );
 
 export function prepareData() {
-    this.totalMetric = d3.sum(this.dataset, d => d[this.metricAccessor]);
+  this.totalMetric = d3.sum(this.dataset, d => d[this.metricAccessor]);
 
-    // Create argumaents based on dimensions
-    let rollupDim = [];
-    this.dimensionsAccessor.forEach(item => {
-        rollupDim.push(d => d[item]);
+  // Create argumaents based on dimensions
+  let rollupDim = [];
+  this.dimensionsAccessor.forEach(item => {
+    rollupDim.push(d => d[item]);
+  });
+
+  const nestedMap = d3.rollup(
+    this.dataset,
+    v => d3.sum(v, d => d[this.metricAccessor]),
+    ...rollupDim
+  );
+
+  // #### reconverting to flat structure makes no sense - but I can do it if I want
+  const flatArray = [];
+  const parentArray = ["root"];
+  let depth = 0;
+  let itemID = 0;
+  let parentID = [0];
+
+  function convertNestdMap(map) {
+    depth++;
+
+    const convertedMap = Array.from(map);
+    convertedMap.forEach((d, i) => {
+      const obj = {};
+      itemID++;
+      obj.id = itemID;
+      obj.parentID = parentID[depth - 1];
+      obj.parent = parentArray[depth - 1];
+      obj.key = d[0];
+      obj.level = depth;
+
+      d.forEach((item, index) => {
+        if (index == 1) {
+          if (item instanceof Map) {
+            parentArray[depth] = d[0];
+            parentID[depth] = itemID;
+            convertNestdMap(item);
+          } else {
+            obj.value = item;
+          }
+        }
+      });
+
+      flatArray.push(obj);
+
+      if (i == convertedMap.length - 1) {
+        depth--;
+      }
     });
+  }
 
-    const nestedMap = d3.rollup(
-        this.dataset,
-        v => d3.sum(v, d => d[this.metricAccessor]),
-        ...rollupDim
-    );
+  convertNestdMap(nestedMap);
+  flatArray.unshift({ id: 0 });
 
-    // #### reconverting to flat structure makes no sense - but I can do it if I want
-    const flatArray = [];
-    const parentArray = ["root"];
-    let depth = 0;
-    let itemID = 0;
-    let parentID = [0];
+  // #### reconverting END
 
-    function convertNestdMap(map) {
-        depth++;
+  const tree = d3
+    .stratify()
+    .id(d => d.id)
+    .parentId(d => d.parentID)(flatArray);
 
-        const convertedMap = Array.from(map);
-        convertedMap.forEach((d, i) => {
-            const obj = {};
-            itemID++;
-            obj.id = itemID;
-            obj.parentID = parentID[depth - 1];
-            obj.parent = parentArray[depth - 1];
-            obj.key = d[0];
-            obj.level = depth;
-
-            d.forEach((item, index) => {
-                if (index == 1) {
-                    if (item instanceof Map) {
-                        parentArray[depth] = d[0];
-                        parentID[depth] = itemID;
-                        convertNestdMap(item);
-                    } else {
-                        obj.value = item;
-                    }
-                }
-            });
-
-            flatArray.push(obj);
-
-            if (i == convertedMap.length - 1) {
-                depth--;
-            }
-        });
-    }
-
-    convertNestdMap(nestedMap);
-    flatArray.unshift({ id: 0 });
-
-    // #### reconverting END
-
-    const tree = d3
-        .stratify()
-        .id(d => d.id)
-        .parentId(d => d.parentID)(flatArray);
-
-    return tree;
+  return tree;
 }
 
 export function partitionData(data) {
-    return d3.partition().size([2 * Math.PI, this.dimensions.boundedRadius])(
-        d3
-            .hierarchy(data)
-            .sum(d => (d.children ? 0 : d.data.value))
-            .sort((a, b) => b.value - a.value)
-    );
+  return d3.partition().size([2 * Math.PI, this.dimensions.boundedRadius])(
+    d3
+      .hierarchy(data)
+      .sum(d => (d.children ? 0 : d.data.value))
+      .sort((a, b) => b.value - a.value)
+  );
 }
 
 export function getParentArray(clickedNode) {
-    const parentArray = [""];
+  const parentArray = [""];
 
-    function getParent(node) {
-        if (node.parent) {
-            if (node.data.data.key) {
-                parentArray.push(node.data.data.key)
-            }        
-            getParent(node.parent)
-        }        
+  function getParent(node) {
+    if (node.parent) {
+      if (node.data.data.key) {
+        parentArray.push(node.data.data.key);
+      }
+      getParent(node.parent);
     }
-    getParent(clickedNode);
+  }
+  getParent(clickedNode);
 
-    return parentArray.reverse()
+  return parentArray.reverse();
 }
 
 export default prepareData;
--- viz/sunburst-drilldown/sunburst/sunburst.js
+++ viz/sunburst-drilldown/sunburst/sunburst.js
@@ -21,46 +21,46 @@ const minSize = 450;
 */
 
 function drawViz(rawData) {
-    // First Error check: component size
-    prepareDOM();
-    const dim = Helper.getMaxDimensions("body");
-    if (dim.max < minSize) {
-        renderErrorMessage(msg.resizeTitle, msg.sizeError);
-        console.log(`current ${dim.max}`);
-        console.log(`required ${minSize}`);
-        return;
-    }
+  // First Error check: component size
+  prepareDOM();
+  const dim = Helper.getMaxDimensions("body");
+  if (dim.max < minSize) {
+    renderErrorMessage(msg.resizeTitle, msg.sizeError);
+    console.log(`current ${dim.max}`);
+    console.log(`required ${minSize}`);
+    return;
+  }
 
-    const data = convertData(rawData);
-    if (data) {
-        const root = prepareDOM();
-        createTooltip(root);
+  const data = convertData(rawData);
+  if (data) {
+    const root = prepareDOM();
+    createTooltip(root);
 
-        const sunburst = new Sunburst("body", data.tables);
+    const sunburst = new Sunburst("body", data.tables);
 
-        // Dimensions and Metric of viz
-        sunburst.metricAccessor = rawData.fields.metric[0].name;
-        sunburst.dimensionsAccessor = [];
-        rawData.fields.dimension.forEach((d, i) => {
-            sunburst.dimensionsAccessor[i] = d.name;
-        });
+    // Dimensions and Metric of viz
+    sunburst.metricAccessor = rawData.fields.metric[0].name;
+    sunburst.dimensionsAccessor = [];
+    rawData.fields.dimension.forEach((d, i) => {
+      sunburst.dimensionsAccessor[i] = d.name;
+    });
 
-        // Color Scheme
-        sunburst.colorScheme = rawData.style.arcColors.value;
-        sunburst.colorSchemeReversed = rawData.style.colorSchemeReversed.value;
+    // Color Scheme
+    sunburst.colorScheme = rawData.style.arcColors.value;
+    sunburst.colorSchemeReversed = rawData.style.colorSchemeReversed.value;
 
-        // Font Color
-        sunburst.fontColor = rawData.style.fontColor.value.color;
-        sunburst.fontOpacity = rawData.style.fontColor.value.opacity;
+    // Font Color
+    sunburst.fontColor = rawData.style.fontColor.value.color;
+    sunburst.fontOpacity = rawData.style.fontColor.value.opacity;
 
-        // Draw Legend
-        sunburst.legend = rawData.style.isLegend.value;
+    // Draw Legend
+    sunburst.legend = rawData.style.isLegend.value;
 
-        // Draw the visualization
-        sunburst.draw();
-    } else {
-        renderErrorMessage(msg.loadingTitle, msg.dataError);
-    }
+    // Draw the visualization
+    sunburst.draw();
+  } else {
+    renderErrorMessage(msg.loadingTitle, msg.dataError);
+  }
 }
 
 /*
@@ -70,27 +70,27 @@ function drawViz(rawData) {
  */
 
 function createTooltip(root) {
-    const tooltip = root
-        .append("div")
-        .attr("id", "tooltip")
-        .attr("class", "tooltip");
+  const tooltip = root
+    .append("div")
+    .attr("id", "tooltip")
+    .attr("class", "tooltip");
 
-    tooltip.append("span").attr("id", "title");
-    tooltip.append("span").attr("id", "count");
+  tooltip.append("span").attr("id", "title");
+  tooltip.append("span").attr("id", "count");
 }
 
 function prepareDOM() {
-    const root = d3.select("body").attr("id", "body");
+  const root = d3.select("body").attr("id", "body");
 
-    // Clean everything
-    d3.select("body")
-        .selectAll("svg")
-        .remove();
+  // Clean everything
+  d3.select("body")
+    .selectAll("svg")
+    .remove();
 
-    d3.select("#error").remove();
-    d3.select("#tooltip").remove();
+  d3.select("#error").remove();
+  d3.select("#tooltip").remove();
 
-    return root;
+  return root;
 }
 
 /*
@@ -99,66 +99,63 @@ function prepareDOM() {
  ** Return object with different sets (data + fields, style, theme, interactions)
  */
 function convertData(dsObj) {
-    const tableData = dsObj.tables.DEFAULT;
-    console.log(`Dataset contains ${tableData.length} records`);
+  const tableData = dsObj.tables.DEFAULT;
+  console.log(`Dataset contains ${tableData.length} records`);
 
-    if (tableData.length == 0) {
-        return;
-    }
+  if (tableData.length == 0) {
+    return;
+  }
 
-    // CONVERT DS data format into D3 style
-    const fields = dsObj.fields.dimension.concat(dsObj.fields.metric);
+  // CONVERT DS data format into D3 style
+  const fields = dsObj.fields.dimension.concat(dsObj.fields.metric);
 
-    const data = [];
-    tableData.map(d => {
-        const row = d.dimension.concat(d.metric);
-        const obj = {};
-        fields.forEach((f, i) => {
-            obj[f.name] = row[i];
-        });
-        data.push(obj);
+  const data = [];
+  tableData.map(d => {
+    const row = d.dimension.concat(d.metric);
+    const obj = {};
+    fields.forEach((f, i) => {
+      obj[f.name] = row[i];
     });
+    data.push(obj);
+  });
 
-    const returnObj = {};
-    returnObj.tables = data;
-    returnObj.fields = fields;
-    returnObj.style = dsObj.style;
-    returnObj.theme = dsObj.theme;
-    returnObj.interactions = dsObj.interactions;
-    return returnObj;
+  const returnObj = {};
+  returnObj.tables = data;
+  returnObj.fields = fields;
+  returnObj.style = dsObj.style;
+  returnObj.theme = dsObj.theme;
+  returnObj.interactions = dsObj.interactions;
+  return returnObj;
 }
 
 /* try catch only for DS */
 function draw(data) {
-    if (LOCAL) {
-        drawViz(data);
-    } else {
-        try {
-            drawViz(data);
-        } catch (err) {
-            renderErrorMessage(msg.errorTitle, `${msg.generalError} ${err}`);
-        }
+  if (LOCAL) {
+    drawViz(data);
+  } else {
+    try {
+      drawViz(data);
+    } catch (err) {
+      renderErrorMessage(msg.errorTitle, `${msg.generalError} ${err}`);
     }
+  }
 }
 
 /* Load data (LOCAL) or take from Google DS */
 async function sunburst() {
-    if (window.location.hostname == LOCAL) {
-        const theDataSet = await d3.json("./data/DS-data-documentation.json");
-        draw(theDataSet);
-    } else {
-        dscc.subscribeToData(draw, { transform: dscc.objectTransform });
-    }
+  if (window.location.hostname == LOCAL) {
+    const theDataSet = await d3.json("./data/DS-data-documentation.json");
+    draw(theDataSet);
+  } else {
+    dscc.subscribeToData(draw, { transform: dscc.objectTransform });
+  }
 }
 
 sunburst();
-window.addEventListener('resize', _.debounce(sunburst, 300));
+window.addEventListener("resize", _.debounce(sunburst, 300));
 
 // ** ERROR HANDLING **
 function renderErrorMessage(errTitle, errMsg) {
-    const root = prepareDOM();
-    Helper.renderErrorMessage(root, errTitle, errMsg);
+  const root = prepareDOM();
+  Helper.renderErrorMessage(root, errTitle, errMsg);
 }
-
-
-
