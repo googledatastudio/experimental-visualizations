6fe77208273e241d77ad46e69438e2d05774915b
Yulan Lin <yulan.lin.2014@gmail.com>
2020-01-06 09:47:22 -0800

    trying again

--- viz/sunburst/src/index.ts
+++ viz/sunburst/src/index.ts
@@ -21,7 +21,7 @@ const styleVal: any = (message: any, styleId: string) => {
   return message.style[styleId].value !== undefined
     ? message.style[styleId].value
     : message.style[styleId].defaultValue;
-}
+};
 
 // TODO just use string formatting and object properties to simplify
 const colorSwitch = (selectedScheme: string) => {
