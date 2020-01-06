818bb69059357adbd30e1af65cae69ee6a2b7419
yulan lin <y3l2n@users.noreply.github.com>
2020-01-06 10:15:42 -0800

    Prettier (#65)
    * fixed color parsing
    
    * making everything conform to prettier

--- viz/heatmap/src/index.js
+++ viz/heatmap/src/index.js
@@ -54,11 +54,10 @@ const buildTooltip = (d, fields) => {
 
 const styleVal = (message, styleId) => {
   // to account for color styling
-  if (
-    message.style[styleId].value !== undefined &&
-    typeof message.style[styleId].value === 'object'
-  ) {
-    return message.style[styleId].value.color;
+  if (typeof message.style[styleId].defaultValue === 'object') {
+    return message.style[styleId].value.color !== undefined
+      ? message.style[styleId].value.color
+      : message.style[styleId].defaultValue.color;
   }
   return message.style[styleId].value !== undefined
     ? message.style[styleId].value
@@ -74,6 +73,7 @@ const colors = message => {
 
 // write viz code here
 const draw = message => {
+  console.log(message.style);
   const margin = {left: 20, right: 20, top: 20, bottom: 20};
 
   const emptyCanvas = d3.select('svg').remove();
