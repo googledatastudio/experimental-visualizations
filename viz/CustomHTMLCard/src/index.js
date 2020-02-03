cff293b8581c80dc6516591688b995a12f2dd3c0
praveena-pappu <lpappu@google.com>
2020-02-03 15:02:14 -0800

    Submit custom HTML card to github

---
+++ viz/CustomHTMLCard/src/index.js
@@ -0,0 +1,59 @@
+const dscc = require('@google/dscc');
+const $ = require('jquery');
+
+export const LOCAL = false;
+
+function drawViz(data){
+  var nameData =data.fields.chartDimension;
+  var rowData = data.tables.DEFAULT;
+  
+  var style = data.style.pageStyle.value;
+  
+  var dimensionData = null;
+  
+  var mainDiv =$("<div>");
+  var fieldArray=[];
+  var fieldData="";
+  var styleTag = $("<style id='styleId'>");
+  
+  $(document).ready(function() {
+    if($('body').hasClass('container')) { 
+      $("body").empty();
+      $("style").remove("#styleId");
+      loadData();
+    }
+    else{
+      loadData();
+    }
+    function loadData(){ 
+      $.each(nameData,function(i,row){
+        fieldData = row.name;
+        fieldArray.push(fieldData);
+      });
+      $.each(rowData,function(j,row){
+        dimensionData = row["chartDimension"];
+        var rowDiv =$("<div class='row'>");
+        for(var k in dimensionData){
+          var finalData = $('<div class="field">');
+          var fieldElement = '<span class="'+fieldArray[k]+'"'+'>'+fieldArray[k]+" - "+'</div>';
+          var data = '<span class="'+dimensionData[k]+'"'+'>'+dimensionData[k]+'</div>';
+          finalData.append(fieldElement+data);
+          rowDiv.append(finalData);
+        }
+        mainDiv.append(rowDiv);
+      });
+      $("body").addClass("container").append(mainDiv);
+    }
+    if(style!=undefined && style!=""){
+      styleTag.append(style);
+      $("head").append(styleTag); 
+    }
+    else{
+      if($('head').has('style')){
+        $("style").remove("#styleId");
+      }
+    }
+  });
+}
+
+dscc.subscribeToData(drawViz, {transform: dscc.objectTransform});
---
+++ viz/CustomHTMLCard/src/localMessage.js
@@ -0,0 +1,51 @@
+/**
+ * This file provides the mock "data" received
+ * by your visualization code when you develop
+ * locally.
+ *
+ */
+export const message = {
+  tables: {
+    DEFAULT: [
+      {
+        dimID: ['25-54'],
+        metricID: [128863172],
+      },
+      {
+        dimID: ['0-14'],
+        metricID: [61175933],
+      },
+      {
+        dimID: ['65+'],
+        metricID: [51055052],
+      },
+      {
+        dimID: ['15-24'],
+        metricID: [43351778],
+      },
+      {
+        dimID: ['55-64'],
+        metricID: [42179856],
+      },
+    ],
+  },
+  fields: {
+    dimID: [
+      {
+        id: 'qt_nzqx6a0xvb',
+        name: 'Age Group',
+        type: 'TEXT',
+        concept: 'DIMENSION',
+      },
+    ],
+    metricID: [
+      {
+        id: 'qt_8isx6a0xvb',
+        name: 'Population',
+        type: 'NUMBER',
+        concept: 'METRIC',
+      },
+    ],
+  },
+  style: {},
+};
