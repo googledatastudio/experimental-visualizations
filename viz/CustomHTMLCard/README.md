5f421914e8f9cd4ca2c6d3bd3371863c35481d9e
praveena-pappu <lpappu@google.com>
2020-02-12 16:49:03 -0800

    update files

--- viz/CustomHTMLCard/README.md
+++ viz/CustomHTMLCard/README.md
@@ -7,5 +7,5 @@ JavaScript visualizations for [Google Data Studio][datastudio].
 
 This visualization helps user to create custom HTML cards using the row data given as input by the user. 
 Each cell from the row is read and the whole row is shown as HTML card to the user. User can customize the HTML cards by providing 
-styling in the "Style" tab. User can mention the styling for the HTML body and row using the tags and class names. The classname for the row data is ".row". Sample styling looks like: "body{background-color:#aebecf} .row{float:left;width:30%;margin:0.5em;border:0.05em solid black;}"
+styling in the "Style" tab. User can mention the styling for the HTML body and row using the tags and class names. The classname for the row data is ".row". Sample styling looks like: https://datastudio.google.com/c/u/0/reporting/1QhuGDiHKUQ-yBs4nfAOesdhr9RBeQPvm/page/NwtEB
 
--- viz/CustomHTMLCard/src/index.js
+++ viz/CustomHTMLCard/src/index.js
@@ -3,67 +3,54 @@ const $ = require('jquery');
 
 export const LOCAL = false;
 
-function drawViz(data) {
-  const nameData = data.fields.chartDimension;
+function drawViz(data){
+  const nameData =data.fields.chartDimension;
   const rowData = data.tables.DEFAULT;
-
-  let style = data.style.pageStyle.value;
-
+  
+  const style = data.style.pageStyle.value;
+  
   let dimensionData = null;
-
-  let mainDiv = $('<div>');
-  let fieldArray = [];
-  let fieldData = '';
+  
+  let mainDiv =$("<div>");
+  let fieldArray=[];
+  let fieldData="";
   let styleTag = $("<style id='styleId'>");
-
+  
   $(document).ready(function() {
-    if ($('body').hasClass('container')) {
-      $('body').empty();
-      $('style').remove('#styleId');
-      loadData();
-    } else {
+    if($('body').hasClass('container')) { 
+      $("body").empty();
+      $("style").remove("#styleId");
       loadData();
     }
-    function loadData() {
-      $.each(nameData, function(i, row) {
+    else{
+      loadData();
+    }
+    function loadData(){ 
+      $.each(nameData,function(i,row){
         fieldData = row.name;
         fieldArray.push(fieldData);
       });
-      $.each(rowData, function(j, row) {
-        dimensionData = row['chartDimension'];
-        var rowDiv = $("<div class='row'>");
-        for (var k in dimensionData) {
-          let finalData = $('<div class="field">');
-          let fieldElement =
-            '<span class="' +
-            fieldArray[k] +
-            '"' +
-            '>' +
-            fieldArray[k] +
-            ' - ' +
-            '</div>';
-          let data =
-            '<span class="' +
-            dimensionData[k] +
-            '"' +
-            '>' +
-            dimensionData[k] +
-            '</div>';
-          finalData.append(fieldElement + data);
+      $.each(rowData,function(j,row){
+        dimensionData = row["chartDimension"];
+        let rowDiv =$("<div class='row'>");
+        for(const k in dimensionData){
+          const finalData = $('<div class="field">');
+          const fieldElement = '<span class="'+fieldArray[k]+'"'+'>'+fieldArray[k]+" - "+'</div>';
+          let data = '<span class="'+dimensionData[k]+'"'+'>'+dimensionData[k]+'</div>';
+          finalData.append(fieldElement+data);
           rowDiv.append(finalData);
         }
         mainDiv.append(rowDiv);
       });
-      $('body')
-        .addClass('container')
-        .append(mainDiv);
+      $("body").addClass("container").append(mainDiv);
     }
-    if (style != undefined && style != '') {
+    if(style!==undefined && style!==""){
       styleTag.append(style);
-      $('head').append(styleTag);
-    } else {
-      if ($('head').has('style')) {
-        $('style').remove('#styleId');
+      $("head").append(styleTag); 
+    }
+    else{
+      if($('head').has('style')){
+        $("style").remove("#styleId");
       }
     }
   });
