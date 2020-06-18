c46b2817c89fa30f0d9473c6cb6d7ad336011fa0
duncan-ross <44205757+duncan-ross@users.noreply.github.com>
2020-06-18 15:01:30 -0400

    Entire Radar viz PR + 'No Data' msg (#84)
    * Add ApexChart Radar chart by Duncan Ross
    
    Co-authored-by: Duncan Ross <duncanross@google.com>

---
+++ viz/Apex-Radar/README.md
@@ -0,0 +1,35 @@
+# Data Studio Community ApexCharts Radar Visualization
+
+This visualization was created using [ApexCharts.js][ApexCharts.js] and was adapted to work with [Google Data Studio][datastudio] using [dscc][dscc]. The radar/spider chart is a web-shaped diagram used to indicate the relative influence of different numerical parameters.
+
+![Radar Chart](./resources/radar.png)
+
+## About this visualization
+
+The [ApexCharts Radar][ApexCharts-Radar] chart community visualization takes 1 dimension and up to 5 metrics. It works best with dimensions of moderate cardinality - for example, dimensions with 5-10 unique values.
+
+## Deployed Version
+Component ID of the deployed version of this visualization:
+
+```bash
+gs://duncanross-test/radar-prod
+```
+
+### Authors
+
+This code was written by Duncan Ross.
+
+### Additional Links
+
+[ApexCharts][ApexCharts]
+
+[Data Studio Community Visualizations][community-viz]
+
+[community-viz]: http://developers.google.com/datastudio/visualization
+[datastudio]: https://datastudio.google.com
+[dscc]:https://github.com/googledatastudio/tooling
+[ApexCharts-Radar]: https://apexcharts.com/docs/chart-types/radar/
+[ApexCharts.js]:https://github.com/apexcharts/apexcharts.js
+[ApexCharts]:https://apexcharts.com/
+
+
---
+++ viz/Apex-Radar/webpack.config.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const path = require('path');
+const fs = require('fs');
+
+const plugins = [];
+let body = '<script src="main.js"></script>';
+
+const iframeHTML = `
+<!doctype html>
+<html><body>
+${body}
+</body></html>
+`;
+
+fs.writeFileSync(path.resolve(__dirname, 'dist', 'vizframe.html'), iframeHTML);
+
+module.exports = [
+  {
+    mode: 'development',
+    entry: './src/index.ts',
+    devServer: {
+      contentBase: './dist',
+    },
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: plugins,
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+  },
+];
