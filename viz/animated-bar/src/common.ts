7d4aa31b9ba270a3feeb61ef70fd68ffb34ba25d
duncan-ross <44205757+duncan-ross@users.noreply.github.com>
2020-08-03 13:40:59 -0400

    05 add license (#107)
    * Added licensing for all motion chart files

--- viz/animated-bar/src/common.ts
+++ viz/animated-bar/src/common.ts
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import {ObjectRow} from '@google/dscc';
 import * as d3 from 'd3';
 
@@ -65,7 +81,6 @@ export function processData(vizData: ObjectRow[],k:number) {
     const keyframes = processKeyFrames(dataMap, k);
     return { keyframes};
 };
-
 
 /**
  * Returns a randomly generated 6 character hex color code.
